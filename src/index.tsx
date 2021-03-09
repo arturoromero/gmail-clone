1e90d136e671862a2a9b8a8e4ec84c64a888edee
Arturo <arturo@rrwebsolution.com>
2021-03-09 16:41:16 -0600

    removed unused module

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-declare module "*.png";
 
 ReactDOM.render(
   <React.StrictMode>
